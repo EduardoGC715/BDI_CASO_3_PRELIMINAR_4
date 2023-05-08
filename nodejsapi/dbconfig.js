6ad07a2d34cc5ebd529334d8baa83b06fa27f125
DadoGC715 <jose.gutierrez.conejo@gmail.com>
2023-05-08 10:39:52 -0600

    change to config server

--- nodejsapi/dbconfig.js
+++ nodejsapi/dbconfig.js
@@ -1,11 +1,12 @@
 const config = {
-    user: 'dbuser',
+    user: 'DBUSER',
     password: '123456789',
-    server: 'localhost',
+    server: 'localhost\\MSSQLSERVER',
     database: 'Esencial Verde',
     options: {
         trustedconnection: true,
         enableArithAort: true,
+        encrypt: false,
         instanceName: 'DESKTOP-SV277U6'
     },
     port: 1433
